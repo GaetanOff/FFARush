6fbd7db2a40b6ff07a356c086e17cc01f4d17893
UnknownMyName <61387206+UnknownMyName@users.noreply.github.com>
2020-11-27 10:37:57 +0100

    pme

--- com/gaetan/ffarush/listener/PlayerListener.java
+++ com/gaetan/ffarush/listener/PlayerListener.java
@@ -140,28 +140,30 @@ public class PlayerListener implements Listener, MovementHandler {
     }
 
     @Override
-    public void handleUpdateLocation(final Player player, final Location location, final Location location1, final PacketPlayInFlying packetPlayInFlying) {
-        final PlayerData playerData = this.ffaRushPlugin.getPlayer(player);
+    public void handleUpdateLocation(final Player player, final Location from, final Location to, final PacketPlayInFlying packetPlayInFlying) {
+        if (from.getX() != to.getX() || from.getZ() != to.getZ()) {
+            final PlayerData playerData = this.ffaRushPlugin.getPlayer(player);
 
-        if (playerData.getPlayerState() == PlayerState.FIGHTING) {
-            if (player.getLocation().getY() < 29) {
-                playerData.inject();
-                if (this.ffaRushPlugin.getVoidPlayers().get(player.getUniqueId()) != null) {
-                    final Player attacker = Bukkit.getPlayer(this.ffaRushPlugin.getVoidPlayers().get(player.getUniqueId()));
+            if (playerData.getPlayerState() == PlayerState.FIGHTING) {
+                if (player.getLocation().getY() < 29) {
+                    playerData.inject();
+                    if (this.ffaRushPlugin.getVoidPlayers().get(player.getUniqueId()) != null) {
+                        final Player attacker = Bukkit.getPlayer(this.ffaRushPlugin.getVoidPlayers().get(player.getUniqueId()));
 
-                    Message.tellToEveryone(Message.RED + player.getName() + Message.GRAY + Lang.VOID_DEATH.getText() + Message.GREEN + attacker.getName() + Message.GRAY + ".");
-                    attacker.setStatistic(Statistic.PLAYER_KILLS, attacker.getStatistic(Statistic.PLAYER_KILLS) + 1);
-                    attacker.setHealth(20.0);
-                    player.setStatistic(Statistic.DEATHS, player.getStatistic(Statistic.DEATHS) + 1);
-                    this.ffaRushPlugin.getVoidPlayers().remove(player.getUniqueId());
-                    return;
+                        Message.tellToEveryone(Message.RED + player.getName() + Message.GRAY + Lang.VOID_DEATH.getText() + Message.GREEN + attacker.getName() + Message.GRAY + ".");
+                        attacker.setStatistic(Statistic.PLAYER_KILLS, attacker.getStatistic(Statistic.PLAYER_KILLS) + 1);
+                        attacker.setHealth(20.0);
+                        player.setStatistic(Statistic.DEATHS, player.getStatistic(Statistic.DEATHS) + 1);
+                        this.ffaRushPlugin.getVoidPlayers().remove(player.getUniqueId());
+                        return;
+                    }
+
+                    Message.tellToEveryone(Message.RED + player.getName() + Message.GRAY + Lang.VOID_DEATH_ALONE.getText());
                 }
-
-                Message.tellToEveryone(Message.RED + player.getName() + Message.GRAY + Lang.VOID_DEATH_ALONE.getText());
+            } else if (playerData.getPlayerState() == PlayerState.SPECTATING) {
+                if (player.getLocation().getY() < 29)
+                    player.teleport(this.ffaRushPlugin.getManagerHandler().getLocationManager().getSpectatorLocation());
             }
-        } else if (playerData.getPlayerState() == PlayerState.SPECTATING) {
-            if (player.getLocation().getY() < 29)
-                player.teleport(this.ffaRushPlugin.getManagerHandler().getLocationManager().getSpectatorLocation());
         }
     }
 
